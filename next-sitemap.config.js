9ea185b133d0b5c9207f0bfc4d15d0c91e880f47
Ahmed Jan <janahmed902@gmail.com>
2026-08-12 15:52:29 +0530

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://seominer.vercel.app',
+  generateRobotsTxt: true, // (optional) generates robots.txt automatically
+  // ...other options
+}
