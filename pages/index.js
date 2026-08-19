10cfc50ef13ec43cf644a1b6534dfd402c5872e9
Ahmed Jan <janahmed902@gmail.com>
2026-08-19 15:54:33 +0530

    update adsense code

--- pages/index.js
+++ pages/index.js
@@ -86,7 +86,7 @@ export default function Home() {
         if (typeof window === 'undefined' || document.querySelector('#adsense-script')) return;
         const script = document.createElement('script');
         script.id = 'adsense-script';
-        script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8393566924928419';
+        script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7347799068802683';
         script.async = true;
         script.crossOrigin = 'anonymous';
         document.head.appendChild(script);
